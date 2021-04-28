b54471f7d3364a67688f266aca3ef9e64117a34c
tumikoto <62601520+tumikoto@users.noreply.github.com>
2021-04-28 15:30:18 -0400

    Update Check-PulseSecureVersion.ps1

--- Check-PulseSecureVersion.ps1
+++ Check-PulseSecureVersion.ps1
@@ -1,6 +1,6 @@
 Param(
-		[parameter(Mandatory=$true)]
-		[String]$targetlistfile
+	[parameter(Mandatory=$true)]
+	[String]$targetlistfile
 )
 
 If (!($targetlistfile)) {
@@ -16,22 +16,22 @@ $targetlist = Get-Content -Path $targetlistfile
 
 foreach ($target in $targetlist) {
 
-  try {
-    $response = Invoke-WebRequest -Method GET -Uri ($target + "/dana-na/nc/nc_gina_ver.txt") -SkipCertificateCheck -ErrorAction Stop
-  } catch {
-    Write-Host -Foregroundcolor Red [+] Target $target version could not be detected`, request error
-    Continue
-  }
+	try {
+		$response = Invoke-WebRequest -Method GET -Uri ($target + "/dana-na/nc/nc_gina_ver.txt") -SkipCertificateCheck -ErrorAction Stop
+	} catch {
+		Write-Host -Foregroundcolor Red [+] Target $target version could not be detected`, request error
+		Continue
+	}
 
-  $product = $response.Content -split "`r`n" | Where {$_ -match '<PARAM NAME="ProductName'} | Select -First 1
-  $Version = $response.Content -split "`r`n" | Where {$_ -match '<PARAM NAME="ProductVersion'} | Select -First 1
+	$product = $response.Content -split "`r`n" | Where {$_ -match '<PARAM NAME="ProductName'} | Select -First 1
+	$Version = $response.Content -split "`r`n" | Where {$_ -match '<PARAM NAME="ProductVersion'} | Select -First 1
 
-  if ($product -and $version) {
-    $product = ($product -split '"')[3]
-    $version = ($version -split '"')[3]
-    Write-Host -Foregroundcolor Green [+] Target $target is $product with version $version
-    Continue
-  }
+	if ($product -and $version) {
+		$product = ($product -split '"')[3]
+		$version = ($version -split '"')[3]
+		Write-Host -Foregroundcolor Green [+] Target $target is $product with version $version
+		Continue
+	}
 
-  Write-Host -Foregroundcolor Red [+] Target $target version could not be detected`, parsed response with no results
+	Write-Host -Foregroundcolor Red [+] Target $target version could not be detected`, parsed response with no results
 }
